7f228b7d4f8c2a28e1eb543b6ba8e711f887ac35
FKSAxell <kevin.concha.regatto@outlook.com>
2020-07-17 19:24:14 -0500

    se corrige random y se implementa lectura de archivo para obtener en la variable full las 10 lineas del archivo que me interesa

--- mips1.asm
+++ mips1.asm
@@ -11,12 +11,42 @@ newline: .asciiz "\n"
 fin: .asciiz "Fin del Programa"       
 myFile: .asciiz "pokeTypes.txt"      # filename for input
 buffer: .space 1024
+full: .space 1024
         #Declaro main como una funcion global
         .globl main
 		.text
-# La etiqueta main representa el punto de partida		
-main:
-            
+# La etiqueta main representa el punto de partida
+#Registro
+#$s0	file descriptor 
+#$s1    random number
+#$s2    contador
+#$s3	random+9
+#$s4    controlador 
+main:   
+        add $s1,$zero,$zero
+        add $s3,$zero,$zero
+
+        add $t1,$zero,$zero #random controlador de linea
+        
+
+        #GENERANDO RANDOM
+        jal randomNumber
+        add $s1,$v0, $zero
+        move $a0,$s1
+        li $v0, 1
+        syscall
+        jal enter
+        #limite random+9
+        addi $s3,$s1,9
+        move $a0,$s3
+        li $v0, 1
+        syscall
+        jal enter
+
+        #iniciando el controlador (para escoger las 10 lineas)
+        addi $t1, $s1, -1
+
+
         #ABRIENDO ARCHIVO
         li   $v0, 13          # system call for open file
         la   $a0, myFile      # input file name
@@ -24,6 +54,8 @@ main:
         li   $a2, 0           # mode is ignored
         syscall               # open a file 
         move $s0, $v0         # save the file descriptor 
+        
+        add $s2,$zero,$zero # declarando contador de lineas
 	loop:
         	# reading from file just opened
         	li   $v0, 14        # system call for reading from file
@@ -33,21 +65,48 @@ main:
         	syscall             # read from file
         	beq $v0,$zero,exit #SALIDA DEL BUCLE AL LEER TODO EL ARCHIVO
 		lb $t5, 0($a1) #CARGANDO EL CARACTER QUE LEE EL BUFFER
-		bne $t5,0xD,nocounter #COMPARANDO SI ES ENTER 
-        	addi $s3,$s3,1 #i+=1 #CONTANDO +1
-        	nocounter: #SALTANDO AL OTRO LOOP POR SI NO ES UN ENTER
-        	# Printing File Content
-        	#li  $v0, 4          # system Call for PRINT STRING
-        	#la  $a0, buffer     # buffer contains the values
-        	#syscall             # print int
+		bne $t5,0xD,no_counter_concatenate #COMPARANDO SI ES ENTER 
+        		addi $s2,$s2,1 #contador+=1
+                        bne $t1,$s2, no_counter_concatenate #si t1 == s2 estamos en la primera linea que necesitamos
+                                #---comienzo de lectura de las 10 lineas:---
+                                #se crea el contador que solo tiene que llegar a 10:
+                                add $t2,$zero,$zero
+                                addi $t3,$zero,10
+                                loop2:
+                                        li   $v0, 14        # system call for reading from file
+                                        move $a0, $s0       # file descriptor 
+                                        la   $a1, buffer    # address of buffer from which to read
+                                        li   $a2,  1       # hardcoded buffer length <- hasta cuantos caracteres presentar (SE SELECCIONA 1 PARA RECORRER POR CARACTER) 
+                                        syscall             # read from file
+                                        lb $t6, 0($a1) #CARGANDO EL CARACTER QUE LEE EL BUFFER
+                                        bne $t6,0xD,no_counter_si_concatenate  #COMPARANDO SI ES ENTER 
+                                                addi $t2,$t2,1  #contando enter
+                                                beq $t2,$t3, exit #SI ESTO ES IGUAL QUIERE DECIR QUE YA TENGO MIS 10 LINEAS EN FULL
+                                                j loop2
+                                        #concatenando en full
+                                        no_counter_si_concatenate:
+                                                la $a0 full
+                                                la $a1 full
+                                                la $a2 buffer
+                                                jal strcatFirst
+                                                j loop2
+        	no_counter_concatenate: #SALTANDO AL OTRO LOOP POR SI NO ES UN ENTER
+        	        #quiero que esto concatene las 9 lineas que son mas de 9 bucles
         		j loop
       
 	exit:
-	#TERMINAMOS IMPRIMIENDO CONTADOR DE LINEAS (es 108, pero el archivo tiene mas enters)
-	move $a0,$s3
-	li $v0, 1   
-        syscall   
         
+        #IMPRIMIENDO FULL
+	li $v0, 4
+	la $a0,full
+        syscall
+        
+        
+        #CERRANDO ARCHIVO
+        li   $v0, 16       # system call for close file
+	move $a0, $s0      # file descriptor to close
+	syscall            # close file
+	
         #Terminar programa
         li $v0, 10
         syscall
@@ -56,15 +115,10 @@ main:
 #FUNCIONES
 randomNumber:		# funcion randomNumber
         li $v0, 42  	# 42 funcion del sistema para aleatorio
-        li $a1, 109     # Se aumenta a 109 para que salga el 108
+        li $a1, 99     
         syscall     	# en $a0 se guarda aleatorio
-
         addi $t0, $a0, 1  # aumenta para cuestiones de intervalo
-        add $v0, $t0, $zero # retorno v1
-
-        li $v0, 1   #Service 1, print int, esto hay que borrar solo se usa para prueba
-        syscall    
-
+        add $v0, $t0, $zero # retorno v0
         jr $ra
         	
 #call the strcpy function
@@ -89,6 +143,13 @@ strcatFirst:
         j strcatSecond
     endStrcat:
         jr $ra
+
+#Funcion para agregar nueva linea :v 
+enter:
+	li  $v0, 4        
+        la  $a0, newline
+       	syscall
+       	jr $ra
 
 
         
